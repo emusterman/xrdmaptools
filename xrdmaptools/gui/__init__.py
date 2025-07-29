952928ddd5f0e39cbd3a66c9dc6d51d6c717e83a
Evan Musterman <emusterma@xf05id2-ws5.nsls2.bnl.local>
2025-07-29 13:43:39 -0400

    placeholder for future gui development

---
+++ xrdmaptools/gui/__init__.py
@@ -0,0 +1,17 @@
+# Submodule for gui support
+# =====================================================================
+#
+# This submodule contains functions and objects for support a gui
+# implementation of the other xrdmaptools functions
+# No other documentation is currently avialable
+
+__author__ = ['Evan J. Musterman',  'Andrew M. Kiss']
+__contact__ = 'emusterma@bnl.gov'
+__license__ = 'N/A'
+__copyright__ = 'N/A'
+__status__ = 'testing'
+__date__ = "07/29/2025" # MM/DD/YYYY
+
+
+# This is required for wildcard (*) imports
+__all__ = [s for s in dir()]
