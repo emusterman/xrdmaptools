fff905685e73f17524528870730aaebdee91ec57
Evan Musterman <emusterma@xf05id2-ws2.nsls2.bnl.local>
2025-10-17 10:30:11 -0400

    Updating maps

--- batch_processing/20250707_functions.py
+++ batch_processing/20250707_functions.py
@@ -137,43 +137,66 @@ def plot_phase_and_xrf_maps(xdm):
 
 
 import matplotlib.ticker as mticker
+from xrdmaptools.utilities.math import arbitrary_center_of_mass, tth_2_d
 # 161738
-def plot_map_and_xrd(xdm):
+def plot_map_and_xrd(xdm, mask=None):
 
     tth, intensity = xdm.integrate1D_image(xdm.max_image)
-    peaks = find_peaks(intensity, prominence=0.1, width=7, height=0.1)[0]
+    # peaks = find_peaks(intensity, prominence=0.1, width=7, height=0.1)[0]
 
     # rescale_array(intensity, upper=100)
 
-    fig, ax = plt.subplots(1, 2, dpi=300, figsize=(8, 3), layout='tight')
+    fig, ax = plt.subplots(1, 3, dpi=300, figsize=(12, 3))
 
     # XRD
     ax[0].plot(tth, intensity, c='k')
     ax[0].set_xlabel(f'Scattering Angle, 2θ [{xdm.scattering_units}]')
-    # ax[0].set_yticks([])
+    ax[0].tick_params(axis='y', labelleft=False)
     ax[0].set_ylabel('Intensity [a.u.]')
     ax[0].set_title('Maximum Integration')
+    ax[0].set_facecolor('white')
     # ax[0].yaxis.set_major_locator(mticker.MultipleLocator(0.2))
 
     # for peak in peaks:
     #     ax[0].text(tth[peak], intensity[peak], 'hkl', fontsize=8, color='k')
 
-    # Map
+    # Max Map
     cen, wind = 526, 20
     im = ax[1].imshow(np.max(xdm.integrations[..., cen - wind : cen + wind], axis=-1), extent=xdm.map_extent(), vmin=0, vmax=0.1)
     fig.colorbar(im, ax=ax[1])
     ax[1].set_xlabel(f'x-axis [{xdm.position_units}]')
     ax[1].set_ylabel(f'y-axis [{xdm.position_units}]')
-    ax[1].set_title('(012) Intensity')
+    ax[1].set_title('(102) Intensity')
     ax[1].set_aspect('equal')
 
-    # asp0 = np.abs(np.diff(ax[0].get_xlim())[0] / np.diff(ax[0].get_ylim())[0])
-    # print(f'{asp0=}')
-    # asp1 = np.abs(np.diff(ax[1].get_xlim())[0] / np.diff(ax[0].get_ylim())[0])
-    # print(f'{asp1=}')
-    # ax[0].set_aspect(asp0)
+    # COM Map
+    cen, wind = 526, 20
+    com_map = np.zeros(xdm.map_shape)
+    if mask is None:
+        mask = np.ones(xdm.map_shape, dtype=np.bool_)
+    com_map[:] = np.nan
+    for indices in xdm.indices:
+        if not mask[indices]:
+            continue
+        com = arbitrary_center_of_mass(xdm.integrations[*indices, cen - wind : cen + wind], xdm.tth[cen - wind : cen + wind])[0]
+        d = tth_2_d(com, wavelength=xdm.wavelength)
+        strain = (d - 2.985504614485868) / 2.985504614485868
+        com_map[indices] = strain
 
+    com_map *= 1e3
+    ext = np.max(np.abs([np.nanmin(com_map), np.nanmax(com_map)]))
+    ext = 10
+    im = ax[2].imshow(com_map, extent=xdm.map_extent(), vmin=-ext, vmax=ext, cmap='Spectral_r')
+    fig.colorbar(im, ax=ax[2])
+    ax[2].set_xlabel(f'x-axis [{xdm.position_units}]')
+    ax[2].set_ylabel(f'y-axis [{xdm.position_units}]')
+    ax[2].set_title('(102) Strain')
+    ax[2].set_aspect('equal')
+    ax[2].set_facecolor('white')
+
+    fig.set_facecolor('none')
     fig.show()
+    return fig
 
 from xrdmaptools.geometry.geometry import modular_azimuthal_shift, modular_azimuthal_reshift
 def plot_azimuth(image, tth_arr, chi_arr, tth_range, chi_range=None, plotme=True, **kwargs):
