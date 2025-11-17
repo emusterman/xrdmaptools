d0d40b1b0910373bf5fd454de972b18f4675a5e3
SRX Operator <xf05id1@xf05id2-ws5.nsls2.bnl.local>
2025-11-17 14:24:07 -0500

    Better submodule and base class imports

--- xrdmaptools/utilities/__init__.py
+++ xrdmaptools/utilities/__init__.py
@@ -16,5 +16,38 @@ __status__ = 'testing'
 __date__ = "03/14/2024" # MM/DD/YYYY
 
 
+__submodules__ = [
+    'auto_processing',
+    'background_estimators',
+    'gas_references',
+    'image_corrections',
+    'math',
+    'reference_data',
+    'utilities'
+]
+
+__base_classes__ = [
+
+]
+
+
 # This is required for wildcard (*) imports
-__all__ = [s for s in dir()]
+__all__ = [s for s in dir() if not s.startswith('_')]
+__all__ += __submodules__ + __base_classes__
+
+
+# Import submodules as necessary
+def __getattr__(attr):
+    import importlib
+
+    if attr in __base_classes__:
+        mod = importlib.import_module(f'{__name__}.{attr}')
+        # Base classes are brought up one level for convenience
+        globals()[attr] = getattr(mod, attr)
+        return globals()[attr]   
+    elif attr in __submodules__:
+        mod = importlib.import_module(f'{__name__}.{attr}')
+        return mod
+    else:
+        err_str = f"module {__name__} has no attribute {attr}"
+        raise AttributeError(err_str)
