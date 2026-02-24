1a4c65a2d2c775a57b3e5ff04a5d1609e105e053
Evan Musterman <evanmusterman@gmail.com>
2026-02-24 18:28:12 -0500

    Actually adding requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+requirements += ["requirements"]
+
+setup(
+    name="xrdmaptools",
+    version="0.1.0",
+    author="Evan J. Musterman",
+    url="https://github.com/emusterman/xrdmaptools.git",
+    packages=find_packages(),
+    install_requires=requirements,
+    description="Tools for analyzing XRD mapping data.",
+    python_requires='>=3.9',
+)
