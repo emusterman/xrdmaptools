bcb5709b91b3aeef5761a277951e52697cf14ffc
SRX Operator <xf05id1@xf05id2-ws5.nsls2.bnl.local>
2026-03-03 17:19:59 -0500

    Fixed requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-requirements += ["requirements"]
+requirements += ["setuptools"]
 
 setup(
     name="xrdmaptools",
